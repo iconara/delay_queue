6f9a65a0ceef84de31f1d2adf40b7ebb9346c82f
Theo <theo@iconara.net>
2012-10-01 18:00:28 +0200

    Added Java implementation

--- lib/delay_queue.rb
+++ lib/delay_queue.rb
@@ -1,65 +1,157 @@
 # encoding: utf-8
 
-require 'set'
+if RUBY_PLATFORM == 'java'
+  require 'java'
 
+  class DelayQueue
+    java_import 'java.util.TreeMap'
+    java_import 'java.util.HashSet'
 
-class DelayQueue
-  def initialize(clock=Time)
-    @clock = clock
-    @elements = {}
-    @timestamps = SortedSet.new
-    @reverse_elements = Hash.new { |h, k| h[k] = Set.new }
-  end
-  
-  def put(element, timestamp=Time.now.to_i, options={})
-    if @elements[element]
-      return unless options[:force] || timestamp > @elements[element]
-      remove(element)
+    def initialize(clock=Time)
+      @clock = clock
+      @timestamp_to_elements = TreeMap.new
+      @element_to_timestamp = {}
     end
-    @elements[element] = timestamp
-    @reverse_elements[timestamp] << element
-    @timestamps << timestamp
-  end
-  
-  def remove(element)
-    timestamp = @elements.delete(element)
-    return unless timestamp
-    @reverse_elements[timestamp].delete(element)
-    if @reverse_elements[timestamp].empty?
-      @reverse_elements.delete(timestamp)
-      @timestamps.delete(timestamp)
+
+    def put(element, timestamp=Time.now.to_i, options={})
+      existing_timestamp = @element_to_timestamp[element]
+      if !existing_timestamp || existing_timestamp < timestamp || options[:force]
+        remove(element) if existing_timestamp
+        elements = @timestamp_to_elements.get(timestamp)
+        elements ||= HashSet.new
+        elements.add(element)
+        @timestamp_to_elements.put(timestamp, elements)
+        @element_to_timestamp[element] = timestamp
+      end
+    end
+    
+    def remove(element)
+      timestamp = @element_to_timestamp.delete(element)
+      if timestamp
+        elements = @timestamp_to_elements.get(timestamp)
+        elements.remove(element)
+        @timestamp_to_elements.remove(timestamp) if elements.empty?
+      end
+    end
+    
+    def pop(n=1)
+      popped_elements = []
+      loop do
+        entry = @timestamp_to_elements.first_entry
+        break unless entry
+        break if entry.key > @clock.now.to_i
+        elements = entry.value
+        iterator = elements.iterator
+        while iterator.has_next && popped_elements.size < n
+          element = iterator.next
+          @element_to_timestamp.delete(element)
+          iterator.remove
+          popped_elements << element
+        end
+        break unless elements.empty?
+        @timestamp_to_elements.delete(entry.key)
+      end
+      if n == 1
+        popped_elements.first
+      else
+        popped_elements
+      end
+    end
+    
+    def pop_all
+      popped_elements = []
+      cutoff = @timestamp_to_elements.floor_key(@clock.now.to_i)
+      if cutoff
+        loop do
+          entry = @timestamp_to_elements.poll_first_entry
+          elements = entry.value
+          elements.each do |element|
+            @element_to_timestamp.delete(element)
+            popped_elements << element
+          end
+          break if entry.key == cutoff 
+        end
+      end
+      popped_elements
+    end
+    
+    def include?(element)
+      @element_to_timestamp.key?(element)
+    end
+    
+    def to_h
+      @element_to_timestamp.dup
+    end
+
+    def size
+      @element_to_timestamp.size
     end
   end
-  
-  def pop(n=1)
-    elements = peek_all.take(n)
-    elements.each { |e| remove(e) }
-    if n == 1
-      elements.first
-    else
+else
+  require 'set'
+
+  class DelayQueue
+    def initialize(clock=Time)
+      @clock = clock
+      @elements = {}
+      @timestamps = SortedSet.new
+      @reverse_elements = Hash.new { |h, k| h[k] = Set.new }
+    end
+    
+    def put(element, timestamp=Time.now.to_i, options={})
+      if @elements[element]
+        return unless options[:force] || timestamp > @elements[element]
+        remove(element)
+      end
+      @elements[element] = timestamp
+      @reverse_elements[timestamp] << element
+      @timestamps << timestamp
+    end
+    
+    def remove(element)
+      timestamp = @elements.delete(element)
+      return unless timestamp
+      @reverse_elements[timestamp].delete(element)
+      if @reverse_elements[timestamp].empty?
+        @reverse_elements.delete(timestamp)
+        @timestamps.delete(timestamp)
+      end
+    end
+    
+    def pop(n=1)
+      elements = peek_all.take(n)
+      elements.each { |e| remove(e) }
+      if n == 1
+        elements.first
+      else
+        elements
+      end
+    end
+    
+    def pop_all
+      elements = peek_all
+      elements.each { |e| remove(e) }
       elements
     end
-  end
-  
-  def pop_all
-    elements = peek_all
-    elements.each { |e| remove(e) }
-    elements
-  end
-  
-  def include?(element)
-    @elements.key?(element)
-  end
-  
-  def to_h
-    @elements.dup
-  end
+    
+    def include?(element)
+      @elements.key?(element)
+    end
+    
+    def to_h
+      @elements.dup
+    end
 
-private
-  
-  def peek_all
-    now = @clock.now.to_i
-    expired_timestamps = @timestamps.take_while { |ts| ts <= now }
-    expired_timestamps.flat_map { |ts| @reverse_elements[ts].to_a }
+    def size
+      @elements.size
+    end
+
+  private
+    
+    def peek_all
+      now = @clock.now.to_i
+      expired_timestamps = @timestamps.take_while { |ts| ts <= now }
+      expired_timestamps.flat_map { |ts| @reverse_elements[ts].to_a }
+    end
   end
 end
